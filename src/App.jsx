876b82fe44b54d0434028cb1fc0b16dc32e91d84
Harsh Kumar Sinha <hksinha960@gmail.com>
2024-07-25 00:21:17 +0530

    Footer error solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import React from "react";
 import Footer from "./footer";
 function App() {
   return (
     <>
-      <h1 className="text-3xl text-blue-700 font-bold underline">
-        Hello world!
-      </h1>
-      
       <Footer />
     </>
   );
--- src/footer.jsx
+++ src/footer.jsx
@@ -1,20 +1,31 @@
-import "../index.css";
-
 function Footer() {
-    return (
-        <footer className="bg-gray-800 text-white py-4 px-4 flex justify-between items-center">
-          <div className="text-sm">
-            <p>
-              VIT University<br /><br />
-              Vellore, Tamil Nadu<br /><br />
-              India - 632014<br /><br />
-              iste@vit.ac.in<br /><br />
-              +91 83289 26213
-            </p>
-            <a href="https://vit.ac.in/index.html" className="hover:text-gray-400 transition duration-300 ease-in-out">Visit ISTE Main Website</a>
-          </div>
-        </footer>
-      );
-    };
+  return (
+    <footer className="bg-gray-800 text-white py-4 px-4 flex justify-between items-center">
+      <div className="text-sm">
+        <p>
+          VIT University
+          <br />
+          <br />
+          Vellore, Tamil Nadu
+          <br />
+          <br />
+          India - 632014
+          <br />
+          <br />
+          iste@vit.ac.in
+          <br />
+          <br />
+          +91 83289 26213
+        </p>
+        <a
+          href="https://vit.ac.in/index.html"
+          className="hover:text-gray-400 transition duration-300 ease-in-out"
+        >
+          Visit ISTE Main Website
+        </a>
+      </div>
+    </footer>
+  );
+}
 
 export default Footer;
