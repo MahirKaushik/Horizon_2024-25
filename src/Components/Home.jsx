bd2458b28b9e8720500a757c7f53a2d2370dc6e7
Aryan Ramesh Jain <138214350+jainaryan04@users.noreply.github.com>
2024-07-24 22:27:22 +0530

    update styling

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,15 +8,16 @@ function Home() {
             </div>
 
             
-            <div className="flex w-full bg-[#b5ddc2] h-full items-center p-8 h-2/3">
-                <div className="w-2/3">
-                    <span className="block text-2xl font-semibold">One of the Biggest Technical</span><br />
-                    <span className="block text-2xl font-semibold">Fests in India <span>is back in 2024</span></span><br />
-                    <span className="block text-2xl font-semibold mb-4">See you at Horizon!</span><br />
-                    <button type="submit" className="bg-[#F2C6AB] px-4 py-2 rounded-lg text-lg font-bold">Register Now</button>
+            <div className="flex w-full bg-[#b5ddc2] items-center p-8 h-2/3 border-t-black border-b-black border-2">
+                <div className="w-2/3 h-full border-r-black border-r-2">
+                    <br /> <br />
+                    <span className="block text-5xl font-poppins font-light">One of the Biggest Technical</span><br />
+                    <span className="block text-5xl font-poppins ">Fests in India <span className="font-extrabold">is back in 2024</span></span><br />
+                    <span className="block text-3xl mb-4 font-poppins">See you at Horizon!</span><br />
+                    <button type="submit" className="bg-[#F2C6AB] px-6 py-4 border-black border-2 rounded-full text-2xl ">Register Now</button>
                 </div>
-                <div className="w-1/3 flex justify-center">
-                    <img className="h-[50vh]" src="/homepage.png" />
+                <div className="w-1/3 flex ml-14 h-full ">
+                    <img className="h-[55vh] w-[30vw] rounded-lg border-black border-2" src="/homepage.png" />
                 </div>
             </div>
         </div>
