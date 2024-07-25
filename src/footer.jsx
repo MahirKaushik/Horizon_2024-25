a33e3236e4ac1a9aa50a5918bf4dbff5eb28e8ff
1234swatika <swatika2004@outlook.com>
2024-07-25 14:40:35 +0530

    changes 4

--- src/footer.jsx
+++ src/footer.jsx
@@ -1,22 +1,18 @@
+import React from 'react';
+
 function Footer() {
   return (
-    <footer className="bg-gray-800 text-white py-4 px-4 flex justify-between items-center">
-      <div className="text-sm">
-        <p>
-          VIT University
-          <br />
-          <br />
-          Vellore, Tamil Nadu
-          <br />
-          <br />
-          India - 632014
-          <br />
-          <br />
-          iste@vit.ac.in
-          <br />
-          <br />
-          +91 83289 26213
-        </p>
+    <footer
+      className="w-[1440px] h-[970px] top-[8978px] p-[42px_36px_0px_36px] gap-0 border-b-2 border-black opacity-100 bg-[#B5DDC2] text-black flex justify-between items-center"
+    >
+      <div className="text-sm flex-1">
+        <div>VIT University</div>
+        <div>Vellore, Tamil Nadu</div>
+        <div>India - 632014</div>
+        <div>iste@vit.ac.in</div>
+        <div>+91 83289 26213</div>
+      </div>
+      <div className="flex-shrink-0">
         <a
           href="https://vit.ac.in/index.html"
           className="hover:text-gray-400 transition duration-300 ease-in-out"
