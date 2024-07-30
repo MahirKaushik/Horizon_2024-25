c4c15f13ce3035eb592f3fe6e984a4e86e321f99
Aryan Ramesh Jain <138214350+jainaryan04@users.noreply.github.com>
2024-07-30 23:45:41 +0530

    update font and tab view

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,14 +5,14 @@
             <div>
                 <div className="hidden lg:block h-screen w-full overflow-hidden">
                     <div className="h-1/3 w-full bg-[#fefbec] flex justify-center items-center">
-                        <p className="font-main text-[16vw]">HORIZON'24</p>
+                        <p className="font-main text-[13.6vw]">HORIZON'24</p>
                     </div>
                     <div className="flex w-full bg-[#b5ddc2] items-center h-2/3 border-t-black border-b-black border-2">
                         <div className="w-2/3 h-full border-black border-r-2 p-8 m-10">
                             <br /> <br />
                             <span className="block text-5xl font-poppins font-thin">One of the Biggest Technical</span><br />
                             <span className="block text-5xl font-poppins font-thin">Fests in India <span className="font-extrabold">is back in 2024</span></span><br />
-                            <span className="block text-3xl mb-4 font-poppins font-thin">See you at Horizon!</span><br />
+                            <span className="block text-3xl mb-[6vh] font-poppins font-thin">See you at Horizon!</span><br />
                             <button type="submit" className="bg-[#F2C6AB] px-6 py-4 border-black border-2 rounded-full text-2xl ">Register Now</button>
                         </div>
                         <div className="w-1/3 h-full flex items-center relative">
@@ -22,24 +22,24 @@
                 </div>
 
                 <div className="lg:hidden h-screen w-full overflow-hidden">
-                    <div className="h-1/6 w-full bg-[#fefbec] flex justify-center items-center">
-                        <p className="font-main text-[16vw]">HORIZON'24</p>
+                    <div className="h-1/6 md:h-[11vh] w-full bg-[#fefbec] flex justify-center items-center">
+                        <p className="font-main text-[14vw] md:text-[10vw]">HORIZON'24</p>
                     </div>
-                    <div className="w-full bg-[#b5ddc2] items-center h-5/6 border-t-black border-b-black border-2">
+                    <div className="w-full bg-[#b5ddc2] items-center h-screen border-t-black border-b-black border-2">
                         <div className="w-full h-full border-black relative">
-                            <div className="h-[25vh] m-[4vw]">
-                                <br /> <br />
-                                <span className="text-[2.5vh] font-poppins font-thin">One of the Biggest Technical</span><br />
-                                <span className="block my-2 text-[2.5vh] font-poppins font-thin">Fests in India <span className="font-extrabold">is back in 2024</span></span><br />
-                                <span className="text-[2.5vh] font-poppins font-thin block">Sed vestibulum diam auctor, </span>
-                                <span className="text-[2.5vh] mb-4 font-poppins font-thin ">tincidunt <span className="font-extrabold">mi nec</span></span><br />
+                            <div className="h-[25vh] ml-[4vw] mt-[5vh] md:mt-[7vh] md:text-center">
+                                
+                                <span className="text-[2.5vh] md:text-[3.5vh] font-poppins font-thin">One of the Biggest Technical</span><br />
+                                <span className="block my-2 md:my-1 text-[2.5vh] md:text-[3.5vh] font-poppins font-thin">Fests in India <span className="font-extrabold">is back in 2024</span></span><br />
+                                <span className="text-[2.5vh] md:text-[3.5vh] font-poppins font-thin block">Sed vestibulum diam auctor, </span>
+                                <span className="text-[2.5vh] md:text-[3.5vh] mb-4 font-poppins font-thin ">tincidunt <span className="font-extrabold">mi nec</span></span><br />
                             </div>
-                            <div className="h-2/3 ">
+                            <div className="h-2/3">
                                 <div className="w-full  flex justify-center relative">
-                                    <img className="h-[35vh] w-[80vw] mt-8 mb-3" src="/homepage.png" />
+                                    <img className="h-[35vh] md:h-[45vh] w-[80vw] md:w-[60vw] mt-8 mb-3" src="/homepage.png" />
                                 </div>
                                 <div className="w-full flex justify-center">
-                                    <button type="submit" className="bg-[#F2C6AB] px-2 py-2 mt-4 border-black border-2 rounded-lg text-xl">Register Now</button>
+                                    <button type="submit" className="bg-[#F2C6AB] px-2 py-2 mt-[2vh] md:my-6 border-black border-2 rounded-lg text-xl">Register Now</button>
                                 </div>
                             </div>
                         </div>   
