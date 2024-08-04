3fe9f54efc309bea589737054f64a868c5cbd286
raptor7197 <vamsikrishna75836@gmail.com>
2024-08-04 21:57:53 +0530

    Navbar completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Navbar from "./component/Navbar"
+
+
 function App() {
   return (
     <>
       <h1 className="text-3xl text-blue-700 font-bold underline">
         Hello world!
       </h1>
+      <Navbar />
     </>
   );
 }
---
+++ src/fonts/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+
+
+
+
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        customFont: ['"ITC_Tiffany"', "sans-serif"],
+      },
+    },
+  },
+};
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'green-200': '#243c5a',
+      },
+    }
+  }
+}
+
